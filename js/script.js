8a2b77f5d691bdc612993a31d515994bebe69bf9
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-23 16:59:50 +0200

    add tasto aggiungi

--- js/script.js
+++ js/script.js
@@ -2,11 +2,20 @@ const app = new Vue(
     {
         el: '#app',
         data: {
+            newListIt: "",
             list: [
                 'Fare i compiti',
                 'Fare la spesa',
                 'Fare il bucato'
             ]
+        },
+
+        methods: {
+            aggTodo(){
+                // console.log(this.newListIt)
+                this.list.push(this.newListIt);
+                this.newListIt = "";
+            }
         }
     }
 );
